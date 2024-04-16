de48b69851cb0c032c86e13e0895ec7c6180cbcb
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-16 14:27:14 +0200

    create useUserStorage and User component

---
+++ src/components/User.jsx
@@ -0,0 +1,28 @@
+import { useUserStore } from "../stores/useUserStore";
+import { useEffect } from "react";
+
+export const User = () => {
+  const { userData, loading, error, fetchUserData } = useUserStore();
+
+  // const userData = useUserStore(state => state.userData) //Specific selection
+
+  useEffect(() => {
+    fetchUserData();
+  }, [fetchUserData]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  return userData ? (
+    <div>
+<h2>User</h2>
+<p>Name: {userData.name.first} {userData.name.last}</p>
+<img src={userData.picture.medium} alt="User" />
+    </div>
+  ) : null
+};
