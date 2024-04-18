f4fa599a3e2f869d8b4119488e2cd17298f9bc25
Frida Svensson <fridamarisvensson@gmail.com>
2024-04-18 12:03:53 +0200

    learn about set, get and the use of utils

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { User } from "./components/User"
 
 import "./App.css"
+import { SecondCount } from "./components/SecondCount"
 
 export const App = () => {
   return (
@@ -8,6 +9,7 @@ export const App = () => {
       <h1>Zustand fetch practice</h1>
       <p>Create a store with a state that matches User.jsx</p>
       <User />
+      <SecondCount />
     </>
   )
 }
---
+++ src/components/SecondCount.jsx
@@ -0,0 +1,17 @@
+import { logSecondCount } from "../utils/logSecondCount";
+import { useSecondCountStore } from "../stores/useSecondCountStore";
+
+export const SecondCount = () => {
+  const { count, increment, decrement, reset } = useSecondCountStore();
+
+  return (
+    <div>
+      <h2>Second count</h2>
+      <p>Count: {count}</p>
+      <button onClick={increment}> Increment </button>
+      <button onClick={decrement}> Decrement </button>
+      <button onClick={reset}> Reset </button>
+      <button onClick={logSecondCount}> Log </button>
+    </div>
+  );
+};
